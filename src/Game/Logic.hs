a3bc9af4e4daedef97f4ae47e2b73f79be0a94d2
Ben Foppa <benjamin.foppa@gmail.com>
2012-06-20 19:13:42 -0400

    Better bumping bureau

--- src/Game/Logic.hs
+++ src/Game/Logic.hs
@@ -10,6 +10,8 @@ import Prelude ()
 import Util.Prelewd
 
 import Data.Tuple
+import Text.Show
+import Util.Impure
 
 import Types
 
@@ -75,41 +77,50 @@ gravity = Propel (Vector 0 (-9.81)) Nothing
 initState :: GameState
 initState = GameState [ Block (Vector 1 1) (Vector 0 0) (Vector 1 10) [gravity]
                       , Platform (Vector 4 1) (Vector (-3) (-1)) (Vector 2 0) []
+                      , Platform (Vector 1 4) (Vector (-8) (-4)) (Vector 0 0) [Propel (Vector 1 0) (Just 3)]
                       ]
 
-collisionHandler :: Position         -- ^ Original position of first object
+collisionHandler :: DeltaT           -- ^ Time interval of collision
+                 -> Position         -- ^ Original position of first object
                  -> Position         -- ^ Original position of second object
                  -> GameObject       -- ^ Object to update
                  -> GameObject       -- ^ Object it collided with
                  -> GameObject       -- ^ Updated object
-collisionHandler p1 p2 g1 g2 = if' (isBlock g1 || isPlatform g2) (bump p2 g2 p1) g1
+collisionHandler t p1 p2 g1 g2 = if' (isBlock g1 || isPlatform g2) (bump t p2 g2 p1) g1
 
-mindex :: Ord a => Vector a -> Integer
-mindex v = fst $ minimumBy (compare `on` snd) $ (,) <$> vector [0..] <*> v
+-- | Index of the smallest element
+smallestIndex :: (Num a, Ord a) => Vector a -> Integer
+smallestIndex v = fst $ minimumBy (compare `on` abs.snd) $ (,) <$> vector [0..] <*> v
 
-bump :: Position   -- ^ Original position of next parameter
+-- | Minimum parameter, if equal return the first
+minBy :: Ord a => (a -> a -> Ordering) -> a -> a -> a
+minBy f x y = if f x y == GT
+              then y
+              else x
+
+bump :: DeltaT     -- ^ Time duration of bump
+     -> Position   -- ^ Original position of next parameter
      -> GameObject -- ^ Object with which to bump
      -> Position   -- ^ Original position of next parameter
      -> GameObject -- ^ Object to bump
      -> GameObject
-bump r2 g2 r1 g1 = g1
+bump t r2 g2 r1 g1 = g1
         { posn = posn g1 <&> (+) <*> bumpVector
-        , vcty = setV bumpDim 0 $ vcty g1
+        -- Velocity changed, since we shifted position
+        , vcty = vcty g1 <&> (+) <*> fmap (/t) bumpVector
         }
     where
         bumpVector = singleV bumpDim $ overlap!bumpDim
 
-        bumpDim = mindex bumpFactors
+        bumpDim = smallestIndex bumpFactors
         bumpFactors = overlap <&> (/) <*> moveVector
         moveVector = (r1 <&> (-) <*> posn g1) <&> (+) <*> (r2 <&> (-) <*> posn g2)
 
-        overlap = overlap1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2 <*> moveVector
-        overlap1 x1 w1 x2 w2 = prefer ((x2 + w2) - x1) (x2 - (x1 + w1))
-
-        prefer d1 d2 d
-            | signum d == signum d1 = d1
-            | signum d == signum d2 = d2
-            | otherwise             = 0
+        -- Shortest overlap vector (i.e. from the corner of one rectangle to the opposite corner of another)
+        overlap = overlap1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2
+        -- The shortest overlap in 1D
+        overlap1 x1 w1 x2 w2 = preferShorter ((x2 + w2) - x1) (x2 - (x1 + w1))
+        preferShorter = minBy (compare `on` abs)
 
 collision :: GameObject -> GameObject -> Bool
 collision g1 g2 = and $ collision1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2
@@ -120,12 +131,13 @@ collision g1 g2 = and $ collision1 <$> posn g1 <*> size g1 <*> posn g2 <*> size 
                                && x2 + w2 >= x1
 
 -- | If the objects collide, call the appropriate handlers; otherwise just return
-tryCollide :: Position   -- ^ Original position of the object to bump
+tryCollide :: DeltaT     -- ^ Time interval of collision
+           -> Position   -- ^ Original position of the object to bump
            -> Position   -- ^ Original position of the object it collided with
            -> GameObject -- ^ Object to bump
            -> GameObject -- ^ Object it collided with
            -> GameObject -- ^ Updated object
-tryCollide p1 p2 = bool $$ const $* collisionHandler p1 p2 $* collision
+tryCollide t p1 p2 = bool $$ const $* collisionHandler t p1 p2 $* collision
 
 -- | One advancement of physics
 updatePhysics :: DeltaT -> GameObject -> GameObject
@@ -153,15 +165,16 @@ updateInputs is g = g { objects = foldr updateInput (objects g) is }
         updateInput _ = id
 
 -- | Ensure no objects are colliding
-updateBumps :: [(Position, GameObject)] -- ^ [(originalPos, obj)]
+updateBumps :: DeltaT
+            -> [(Position, GameObject)] -- ^ [(originalPos, obj)]
             -> [GameObject]             -- ^ Updated objects
-updateBumps = (snd <$>) . foldr bumpCons []
+updateBumps t = (snd <$>) . foldr bumpCons []
     where
         -- | Cons the object on to the list, as well as bump every other object with it.
         bumpCons obj = (\(x, l) -> x : l) . foldCons collide2 obj
-        collide2 (p1, o1) (p2, o2) = ((p1, tryCollide p1 p2 o1 o2), (p2, tryCollide p2 p1 o2 o1))
+        collide2 (p1, o1) (p2, o2) = ((p1, tryCollide t p1 p2 o1 o2), (p2, tryCollide t p2 p1 o2 o1))
 
 update :: [Input] -> DeltaT -> GameState -> GameState
 update is t g@(GameState { objects = o }) = updateInputs is $ g { objects = bumpedObjects }
     where
-        bumpedObjects = updateBumps $ zip (posn <$> o) $ updatePhysics t <$> o
+        bumpedObjects = updateBumps t $ zip (posn <$> o) $ updatePhysics t <$> o
