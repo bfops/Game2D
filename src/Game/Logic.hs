3cd40df17d44a3661ea140b8df3463600b382b9b
Ben Foppa <benjamin.foppa@gmail.com>
2012-06-20 17:56:45 -0400

    Physics overhaul and input loss

--- src/Game/Logic.hs
+++ src/Game/Logic.hs
@@ -9,23 +9,38 @@ module Game.Logic ( GameObject (..)
 import Prelude ()
 import Util.Prelewd
 
+import Data.Tuple
+
 import Types
 
 type Size = Vector Double
 type Position = Vector Double
 type Velocity = Vector Double
+type Acceleration = Vector Double
+data Propulsion t = Propel Acceleration t
+
+type TimedPropel = Propulsion (Maybe DeltaT)
+
+instance Functor Propulsion where
+    fmap f (Propel a m) = Propel a (f m)
 
 -- | An object in the game world
 data GameObject
     = Block { size     :: Size
             , posn     :: Position
             , vcty     :: Velocity
+            , propels  :: [TimedPropel]
             }
-    | Platform { size   :: Size
-               , posn   :: Position
-               , vcty   :: Velocity
-               , moveT  :: Maybe DeltaT -- ^ Time spent moving; Nothing if not moving
+    | Platform { size       :: Size
+               , posn       :: Position
+               , vcty       :: Velocity
+               , propels    :: [TimedPropel]
                }
+
+accl :: GameObject -> Acceleration
+accl g = foldr (liftA2 (+)) (pure 0) $ withoutT <$> propels g
+    where
+        withoutT (Propel a _) = a
 
 isBlock :: GameObject -> Bool
 isBlock (Block {}) = True
@@ -53,107 +68,100 @@ foldCons :: (b -> a -> (b, a))
          -> (b, [a])
 foldCons f x0 = foldr (\x (b, l) -> (:l) <$> f b x) (x0, []) 
 
+gravity :: TimedPropel
+gravity = Propel (Vector 0 (-9.81)) Nothing
+
 -- | Start state of the game world
 initState :: GameState
-initState = GameState [ Block (Vector 1 1) (Vector 0 0) (Vector 1 10)
-                      , Platform (Vector 4 1) (Vector 0 (-1)) (Vector 0 0) Nothing
+initState = GameState [ Block (Vector 1 1) (Vector 0 0) (Vector 1 10) [gravity]
+                      , Platform (Vector 4 1) (Vector (-3) (-1)) (Vector 2 0) []
                       ]
 
-collisionHandler :: GameObject       -- ^ Object to update
+collisionHandler :: Position         -- ^ Original position of first object
+                 -> Position         -- ^ Original position of second object
+                 -> GameObject       -- ^ Object to update
                  -> GameObject       -- ^ Object it collided with
-                 -> Vector Collision -- ^ Description of the specific collision
                  -> GameObject       -- ^ Updated object
-collisionHandler g1 g2 _ = if' (isBlock g1 || isPlatform g2) (`bump` g2) g1
+collisionHandler p1 p2 g1 g2 = if' (isBlock g1 || isPlatform g2) (bump p2 g2 p1) g1
 
--- | `bump p q` returns `p`, readjusted in position to not overlap `q`.
-bump :: GameObject -> GameObject -> GameObject
-bump p1 p2 = let
-                allBumps = bumps1 <$> posn p1 <*> size p1 <*> posn p2 <*> size p2
-                -- The bumps as vectors
-                vBumps = concat $ fmap . singleV <$> vector [0..] <*> allBumps
-                bumpVector = shortest $ filter backwardBump vBumps
-             in p1 { posn = posn p1 <&> (-) <*> bumpVector
-                   -- Velocity in the bumped direction becomes 0
-                   , vcty = (\b k -> iff (b == 0) k 0) <$> bumpVector <*> vcty p1
-                   }
+mindex :: Ord a => Vector a -> Integer
+mindex v = fst $ minimumBy (compare `on` snd) $ (,) <$> vector [0..] <*> v
+
+bump :: Position   -- ^ Original position of next parameter
+     -> GameObject -- ^ Object with which to bump
+     -> Position   -- ^ Original position of next parameter
+     -> GameObject -- ^ Object to bump
+     -> GameObject
+bump r2 g2 r1 g1 = g1
+        { posn = posn g1 <&> (+) <*> bumpVector
+        , vcty = setV bumpDim 0 $ vcty g1
+        }
     where
-        relativeV = vcty p1 <&> (-) <*> vcty p2
+        bumpVector = singleV bumpDim $ overlap!bumpDim
 
-        shortest = foldr1 shorter
+        bumpDim = mindex bumpFactors
+        bumpFactors = overlap <&> (/) <*> moveVector
+        moveVector = (r1 <&> (-) <*> posn g1) <&> (+) <*> (r2 <&> (-) <*> posn g2)
 
-        -- True if we're not bumping in the direction we're travelling
-        backwardBump :: Position -> Bool
-        backwardBump x = relativeV `dot` x >= 0
+        overlap = overlap1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2 <*> moveVector
+        overlap1 x1 w1 x2 w2 = prefer ((x2 + w2) - x1) (x2 - (x1 + w1))
 
-        bumps1 :: Double -> Double -> Double -> Double -> [Double]
-        bumps1 x1 w1 x2 w2 = [ (x1 + w1) - x2, x1 - (x2 + w2) ]
+        prefer d1 d2 d
+            | signum d == signum d1 = d1
+            | signum d == signum d2 = d2
+            | otherwise             = 0
 
-type Collision = (Bool, Bool) -- ^ Overlap on low and high object borders, respectively.
-
--- | Get collision data in all dimensions
--- Returns Nothing if the objects do not collide
-collision :: GameObject -> GameObject -> Maybe (Vector Collision)
-collision g1 g2 = sequenceA $ collision1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2
+collision :: GameObject -> GameObject -> Bool
+collision g1 g2 = and $ collision1 <$> posn g1 <*> size g1 <*> posn g2 <*> size g2
     where
         -- 1D collision info
-        collision1 :: Double -> Double -> Double -> Double -> Maybe Collision
-        collision1 x1 w1 x2 w2 = colliding x1 w1 x2 w2 `mcond` (x1 <= x2, x1 + w1 >= x2 + w2)
-
-        colliding :: Double -> Double -> Double -> Double -> Bool
-        colliding x1 w1 x2 w2 =  x1 + w1 >= x2
-                              && x2 + w2 >= x1
+        collision1 :: Double -> Double -> Double -> Double -> Bool
+        collision1 x1 w1 x2 w2 =  x1 + w1 >= x2
+                               && x2 + w2 >= x1
 
 -- | If the objects collide, call the appropriate handlers; otherwise just return
-tryCollide :: GameObject -- ^ Object to update
+tryCollide :: Position   -- ^ Original position of the object to bump
+           -> Position   -- ^ Original position of the object it collided with
+           -> GameObject -- ^ Object to bump
            -> GameObject -- ^ Object it collided with
            -> GameObject -- ^ Updated object
-tryCollide = maybe $$ const $* collisionHandler $* collision
+tryCollide p1 p2 = bool $$ const $* collisionHandler p1 p2 $* collision
 
 -- | One advancement of physics
 updatePhysics :: DeltaT -> GameObject -> GameObject
-updatePhysics t = updateVcty . updatePosn
+updatePhysics t = updatePropels . updateVcty . updatePosn
     where
         updatePosn g = g { posn = posn g <&> (+) <*> fmap (*t) (vcty g) }
+        updateVcty g = g { vcty = vcty g <&> (+) <*> fmap (*t) (accl g) }
+        updatePropels g = g { propels = mapMaybe stepPropel $ propels g }
 
-        updateVcty b@(Block { vcty = v }) = b { vcty = gravity $ v }
-        updateVcty g = g
+        stepPropel :: TimedPropel -> Maybe TimedPropel
+        stepPropel (Propel a (Just t0)) = Propel a . Just <$> decTime t0
+        stepPropel p = Just p
 
-        a_g = Vector 0 (-9.81)
-        gravity v = v <&> (+) <*> (a_g <&> (*t))
-
-updateObject :: DeltaT -> GameObject -> GameObject
-updateObject dt p@(Platform { moveT = Just t }) = p { moveT = mfilter (< 0.25) $ Just $ t + dt }
-updateObject _ p@(Platform { moveT = Nothing }) = p { vcty = pure 0 }
-updateObject _ o = o
-
-updateObjects :: DeltaT -> GameState -> GameState
-updateObjects t g@(GameState { objects = o }) = g { objects = updateObject t . updatePhysics t <$> o }
+        -- Decrement the time by `t`
+        -- Returns Nothing on nonpositive values
+        decTime :: DeltaT -> Maybe DeltaT
+        decTime t0 = mcast (> 0) (t0 - t)
 
 updateInputs :: [Input] -> GameState -> GameState
 updateInputs is g = g { objects = foldr updateInput (objects g) is }
     where
         -- Update a list of gameobjects with an input
         updateInput :: Input -> [GameObject] -> [GameObject]
-        updateInput (Move d) = fmap $ bool <*> moveObj d <*> isPlatform
+        updateInput (Move _) = fmap $ bool <*> id <*> isPlatform
         updateInput _ = id
 
-        moveObj :: Direction -> GameObject -> GameObject
-        moveObj d o = o { vcty = moveVector d <&> (+) <*> vcty o
-                        , moveT = Just 0
-                        }
-
-        moveVector Up = Vector 0 2
-        moveVector Right = Vector 3 0
-        moveVector Down = negate <$> moveVector Up
-        moveVector Left = negate <$> moveVector Right
-
 -- | Ensure no objects are colliding
-updateBumps :: GameState -> GameState
-updateBumps g = g { objects = foldr bumpCons [] $ objects g }
+updateBumps :: [(Position, GameObject)] -- ^ [(originalPos, obj)]
+            -> [GameObject]             -- ^ Updated objects
+updateBumps = (snd <$>) . foldr bumpCons []
     where
         -- | Cons the object on to the list, as well as bump every other object with it.
         bumpCons obj = (\(x, l) -> x : l) . foldCons collide2 obj
-        collide2 o1 o2 = (tryCollide o1 o2, tryCollide o2 o1)
+        collide2 (p1, o1) (p2, o2) = ((p1, tryCollide p1 p2 o1 o2), (p2, tryCollide p2 p1 o2 o1))
 
 update :: [Input] -> DeltaT -> GameState -> GameState
-update is t = updateBumps . updateInputs is . updateObjects t
+update is t g@(GameState { objects = o }) = updateInputs is $ g { objects = bumpedObjects }
+    where
+        bumpedObjects = updateBumps $ zip (posn <$> o) $ updatePhysics t <$> o
