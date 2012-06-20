badaf5fc5a3052c0f1e40ec65b2c376ddd335f95
Ben Foppa <benjamin.foppa@gmail.com>
2012-06-19 23:41:36 -0400

    More Vector tests

--- test/Vector.hs
+++ test/Vector.hs
@@ -35,21 +35,31 @@ prop_set v = and $ fmap updateWorks $ indices v
     where
         -- To make sure it's not the same value
         x i = v!i - 1
-        updateWorks i = (setV i (x i) v) ! i == x i
+        updateWorks i = and $ indexMatches i <$> setV i (x i) v <*> vector [0..]
+        indexMatches i k j = (i == j && k == x i) || (i /= j && k == v!j)
 
 prop_single :: Integer -> Bool
 prop_single x = and $ fmap isSingle $ indices $ pure 0
     where
         v i = singleV i x
-        isSingle i = and $ p i <$> vector (indices $ v i) <*> v i
+        isSingle i = and $ p i <$> vector [0..] <*> v i
         p i t a = (i == t && a == x) || (i /= t && a == 0)
 
-prop_short :: (Vector Integer, Vector Integer, Vector Integer) -> Bool
-prop_short (x, y, z) = shorter (shorter x y) z == shorter x (shorter y z)
+prop_shortAssoc :: (Vector Integer, Vector Integer, Vector Integer) -> Bool
+prop_shortAssoc (x, y, z) = shorter (shorter x y) z == shorter x (shorter y z)
 
-prop_dot :: (Vector Integer, Vector Integer, Vector Integer) -> Bool
-prop_dot (x, y, z) =  (dot x (y <&> (+) <*> z)) == dot x y + dot x z
-                   && (dot (x <&> (+) <*> y) z) == dot x z + dot y z
+prop_shortMult :: (Vector Integer, Vector Integer, Integer) -> Property
+prop_shortMult (x, y, c) = c >= 0 ==> ((c*) <$> shorter x y) == shorter ((c*) <$> x) ((c*) <$> y)
+
+prop_dotCommute :: (Vector Integer, Vector Integer) -> Bool
+prop_dotCommute (x, y) = dot x y == dot y x
+
+prop_dotMult :: (Vector Integer, Vector Integer, Integer) -> Bool
+prop_dotMult (v, w, c) = c * dot v w == dot ((c *) <$> v) w
+
+prop_dotAssoc :: (Vector Integer, Vector Integer, Vector Integer) -> Bool
+prop_dotAssoc (x, y, z) =  (dot x (y <&> (+) <*> z)) == dot x y + dot x z
+                        && (dot (x <&> (+) <*> y) z) == dot x z + dot y z
 
 prop_magnitude :: Vector Integer -> Bool
 prop_magnitude v = let c = (-2) :: Integer
