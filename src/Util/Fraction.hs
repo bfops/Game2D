0013d2d60a92bd8b8cced5c524f3740d80c796ed
Ben Foppa <benjamin.foppa@gmail.com>
2012-08-06 18:19:32 -0400

    Error on division by 0

--- src/Util/Fraction.hs
+++ src/Util/Fraction.hs
@@ -7,6 +7,8 @@ import Util.Prelewd
 import Data.Ratio
 import Text.Show
 
+import Util.Impure
+
 data Fraction a = Frac a a
     deriving (Show)
 
@@ -18,9 +20,15 @@ instance Num a => Num (Fraction a) where
     signum (Frac a b) = (Frac `on` signum) a b
     fromInteger i = Frac (fromInteger i) 1
 
-instance Num a => Fractional (Fraction a) where
+instance (Eq a, Num a) => Fractional (Fraction a) where
+    recip (Frac 0 _) = error "Cannot reciprocate 0"
     recip (Frac a b) = Frac b a
     fromRational = (Frac `on` fromInteger) <$> numerator <*> denominator
+
+instance (Num a, Real a) => Real (Fraction a) where
+    toRational (Frac n d) = let rn = toRational n
+                                rd = toRational d
+                            in (numerator rn * denominator rd) % (denominator rn * numerator rd)
 
 instance (Num a, Eq a) => Eq (Fraction a) where
     (Frac n1 d1) == (Frac n2 d2) = (n1 * d2) == (n2 * d1)
