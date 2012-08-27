1548ad361983cf4dcc15676140212b87196d71d3
Clark Gaebel <cgaebel@csclub.uwaterloo.ca>
2012-08-27 16:58:44 -0400

    Added some missing strictness.

--- src/Game/Vector.hs
+++ src/Game/Vector.hs
@@ -30,7 +30,7 @@ dimensions :: Vector Dimension
 dimensions = Vector Width Height
 
 -- | Homogenous vector
-data Vector a = Vector a a 
+data Vector a = Vector !a !a
     deriving (Eq, Show)
 
 instance Num a => Num (Vector a) where
