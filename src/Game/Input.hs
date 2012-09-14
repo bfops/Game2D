ab35c681c2dcf9245d7aec1a816947b560b5f870
Ben Foppa <benjamin.foppa@gmail.com>
2012-09-14 18:43:27 -0400

    Game.Input exports ButtonState

--- src/Game/Input.hs
+++ src/Game/Input.hs
@@ -1,10 +1,12 @@
 -- | Input handling
 module Game.Input ( Input (..)
+                  , ButtonState
                   ) where
 
-import Util.Prelewd hiding (id, empty)
-
 import Text.Show
+
+import Util.Prelewd hiding (id, empty)
+import Wrappers.Events
 
 -- | Input events understood by the game
 data Input = Jump
