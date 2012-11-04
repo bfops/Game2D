899586392afe5a2e7605cad8402ee11e1087737f
Ben Foppa <benjamin.foppa@gmail.com>
2012-11-04 17:18:00 -0500

    pre-commit script fix

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,7 +4,7 @@ rm -rf /tmp/testbuild &&
 mkdir /tmp/testbuild &&
 cp -r `ls -A --color=never | grep -v "^dist$"` /tmp/testbuild/ &&
 cd dist &&
-mkdir /tmp/testbuild/dist
+mkdir /tmp/testbuild/dist &&
 cp -r `ls -A --color=never | grep -v "^build$"` /tmp/testbuild/dist &&
 cd /tmp/testbuild &&
 rm -f .git/hooks/pre-commit &&
