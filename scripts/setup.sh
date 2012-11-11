2173ce63d3b14ae83e49862833524dcae3362cb2
Ben Foppa <benjamin.foppa@gmail.com>
2012-11-11 16:19:02 -0500

    Setup script fails if cabal fails

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cabal install --only-dependencies && cabal configure --enable-tests
-
-rm -f .git/hooks/pre-commit
-chmod +x scripts/pre-commit.sh
+cabal install --only-dependencies &&
+cabal configure --enable-tests &&
+rm -f .git/hooks/pre-commit &&
+chmod +x scripts/pre-commit.sh &&
 ln -f scripts/pre-commit.sh .git/hooks/pre-commit
