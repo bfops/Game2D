abb259b3e0d886458015daec0f2f1a38aeb43544
Ben Foppa <benjamin.foppa@gmail.com>
2012-11-12 14:00:14 -0500

    Basic numeric tests

---
+++ test/Math.hs
@@ -0,0 +1,36 @@
+module Math (test) where
+
+import Prelewd hiding (Down)
+
+import Game.Physics
+
+import Test.Framework
+import Test.Framework.TH
+import Test.Framework.Providers.QuickCheck2
+
+test :: Test
+test = $(testGroupGenerator)
+
+prop_reflex :: PhysicsValue -> Bool
+prop_reflex x = x == x
+
+prop_zeroIdentity :: PhysicsValue -> Bool
+prop_zeroIdentity x = x + 0 == x
+
+prop_negInverse :: PhysicsValue -> Bool
+prop_negInverse x = x - x == 0
+
+prop_addAssoc :: (PhysicsValue, PhysicsValue, PhysicsValue) -> Bool
+prop_addAssoc (x, y, z) = x + (y + z) == (x + y) + z
+
+prop_addCommute :: (PhysicsValue, PhysicsValue) -> Bool
+prop_addCommute (x, y) = x + y == y + x
+
+prop_multZero :: PhysicsValue -> Bool
+prop_multZero x = x * 0 == 0
+
+prop_multIdentity :: PhysicsValue -> Bool
+prop_multIdentity x = x * 1 == x
+
+prop_multCommute :: (PhysicsValue, PhysicsValue) -> Bool
+prop_multCommute (x, y) = x * y == y * x
--- test/Test.hs
+++ test/Test.hs
@@ -7,10 +7,12 @@ import Test.Framework
 import qualified Vector
 import qualified Movement
 import qualified Range
+import qualified Math
 
 main :: SystemIO ()
 main = defaultMain
         [ Vector.test
         , Movement.test
         , Range.test
+        , Math.test
         ]
