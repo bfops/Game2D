fc9bbee8d09c23989d7ec47dd0728e76e22b46d2
Ben Foppa <benjamin.foppa@gmail.com>
2012-12-08 10:52:34 -0500

    Unit math functions and Instances

--- src/Util/Unit.hs
+++ src/Util/Unit.hs
@@ -2,6 +2,9 @@
 module Util.Unit ( Unit (..)
                  , UnitMult
                  , (&*)
+                 , (*&)
+                 , (&/)
+                 , (/&)
                  ) where
 
 import Prelewd
@@ -10,10 +13,17 @@ import Test.QuickCheck
 import Text.Show
 
 infix 9 `Unit`
+infixl 6 &*
+infixl 6 *&
+infixl 6 &/
+infixl 6 /&
 
 -- | `Unit t v` is a unit with a unit in `t`, and a value in `v`
 newtype Unit t v = Unit { unitless :: v }
     deriving (Show, Eq, Ord, Enum, Num, Real, Fractional, Functor)
+
+instance Applicative f => Sequential (Unit t) f a where
+    sequence (Unit v) = pure Unit <*> v
 
 instance Arbitrary v => Arbitrary (Unit t v) where
     arbitrary = Unit <$> arbitrary
@@ -22,3 +32,12 @@ class UnitMult a b c | a b -> c, a c -> b, b c -> a
 
 (&*) :: (UnitMult a b c, Num v) => Unit a v -> Unit b v -> Unit c v
 (&*) a b = Unit $ unitless a * unitless b
+
+(*&) :: (UnitMult a b c, Num v) => Unit b v -> Unit a v -> Unit c v
+(*&) = flip (&*)
+
+(&/) :: (UnitMult a b c, Fractional v) => Unit c v -> Unit a v -> Unit b v
+(&/) c a = Unit $ unitless c / unitless a
+
+(/&) :: (UnitMult a b c, Fractional v) => Unit c v -> Unit b v -> Unit a v
+(/&) c b = Unit $ unitless c / unitless b
