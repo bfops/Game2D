53db2cbdc2a51246e0e7d19228bb162a8e839980
Ben Foppa <benjamin.foppa@gmail.com>
2012-12-29 22:00:12 -0500

    Quicker pre-commits

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-rm -rf /tmp/testbuild &&
-mkdir /tmp/testbuild &&
-cp -r `ls -A --color=never | grep -v "^dist$"` /tmp/testbuild/ &&
-cd dist &&
-mkdir /tmp/testbuild/dist &&
-cp -r `ls -A --color=never | grep -v "^build$"` /tmp/testbuild/dist &&
-cd /tmp/testbuild &&
+dir=/tmp/testbuild/`pwd | sed "s/.*\///g"`
+if [ -d "$dir" ]; then
+    rm -rf "$dir"/.git
+    echo Re-using build environment in "$dir"
+else
+    rm -rf "$dir" &&
+    echo Creating temporary build environment in "$dir"
+fi &&
+
+mkdir -p "$dir"
+cp -r --preserve=timestamps . "$dir" &&
+cd "$dir" &&
 rm -f .git/hooks/pre-commit &&
 git commit -m "Temp" > /dev/null &&
 git reset --hard HEAD > /dev/null &&
-git clean -fd &&
+git clean -fd > /dev/null &&
 scripts/setup.sh &&
 scripts/build.sh &&
 echo &&
 scripts/test.sh &&
 echo &&
 scripts/linecheck.sh &&
-echo
+echo &&
+rm -rf "$dir"
